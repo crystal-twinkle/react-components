b71b11717d627cb571369e7f279a6d88070aca18
Kristalik8 <autoro1@mail.ru>
2023-10-28 20:14:52 +0300

    refactor: change styles

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { IPost } from './models';
+import '../assets/PostList.css';
 
 type PostListProps = {
   posts: IPost[];
@@ -12,13 +13,13 @@ const PostList: React.FC<PostListProps> = ({ posts, title }) => {
   }
 
   return (
-    <div className="list-wrap">
+    <div>
       <h2 style={{ textAlign: 'center' }}>{title}</h2>
       <div className="list">
         {posts.map((post: IPost) => (
           <div className="list__element" key={post.name}>
-            <div className="list__content">
-              <strong> {post.name}</strong>
+            <div>
+              <p className="list__name"> {post.name}</p>
               <div>
                 <img src={post.sprites.front_default} alt="front" />
                 <img src={post.sprites.back_default} alt="back" />
